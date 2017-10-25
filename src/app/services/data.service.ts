00a4fba7a48f4412202bab4a873e27b8a7c3b2e1
mahmoodahmad100 <mahmood073@gmail.com>
2017-10-25 21:52:07 +0200

    get demo posts

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
 
-  constructor() { 
+  constructor(public http:Http) { 
   	console.log('you are ready to go with DataService');
+  }
+
+  getPosts()
+  {
+  	return this.http.get('https://jsonplaceholder.typicode.com/posts').map(res => res.json());
   }
 
 }
