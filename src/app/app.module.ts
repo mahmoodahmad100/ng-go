e68ee41888c8bca645a8679a2e92ff784be856bb
mahmoodahmad100 <mahmood073@gmail.com>
2018-01-10 22:09:41 +0200

    cut pipe & test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,24 @@ import { DataService } from './services/data.service';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
 import { AboutComponent } from './components/about/about.component';
+import { TestComponent } from './test.component';
+
+import { CutPipe } from './cut.pipe';
 
 
 const APPROUTES:Routes = [
   {path:'' , component:UserComponent},  
-  {path:'aboutApp' , component:AboutComponent}
+  {path:'aboutApp' , component:AboutComponent},
+  {path:'test' , component:TestComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    AboutComponent
+    AboutComponent,
+    TestComponent,
+    CutPipe
   ],
   imports: [
     BrowserModule,
