a7cfbfd22c1dea01b4624ad3725c43015927d94b
mahmoodahmad100 <mahmood073@gmail.com>
2018-01-10 23:18:50 +0200

    like func

--- src/app/test.component.ts
+++ src/app/test.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
 	selector: 'test', 
-	template: `{{text | cut:15}}`
+	template: `{{text | cut:15}} <span (click)="like()" class="glyphicon" [class.glyphicon-star]="isLiked" [class.glyphicon-star-empty]="!isLiked"></span>`
 })
-
 
 export class TestComponent
 {
 	text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non aliquam libero, euismod semper mauris. Donec in sollicitudin metus. Aenean non sem massa. Phasellus semper, ante vel fermentum efficitur, massa ex pulvinar arcu, vel semper enim erat eget enim. Maecenas tempor euismod eros";
+
+	isLiked:boolean     = false;
+
+	like()
+	{
+		this.isLiked  = !this.isLiked;
+	}
 }
