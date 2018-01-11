a22eeac68b4de32a3386fc8415a551be3ca83f86
mahmoodahmad100 <mahmood073@gmail.com>
2018-01-11 22:12:54 +0200

    input ... using @Input()

--- src/app/test.component.ts
+++ src/app/test.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
 	selector: 'test', 
@@ -7,15 +7,14 @@ import { Component } from '@angular/core';
 				<br>
 				<input class="form-control" [(ngModel)]="stringInput">
 				{{ stringInput | titleCase }}
-	          `,
-	inputs: ['isLiked']
+	          `
 })
 
 export class TestComponent
 {
 	text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non aliquam libero, euismod semper mauris. Donec in sollicitudin metus. Aenean non sem massa. Phasellus semper, ante vel fermentum efficitur, massa ex pulvinar arcu, vel semper enim erat eget enim. Maecenas tempor euismod eros";
 
-	isLiked:boolean = false;
+	@Input() isLiked:boolean = false;
 	stringInput:string;
 
 	like()
