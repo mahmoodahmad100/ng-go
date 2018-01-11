f451d7b30cfccba46bbb60fbe3670bac41fd10fb
mahmoodahmad100 <mahmood073@gmail.com>
2018-01-11 22:22:04 +0200

    input -> alias

--- src/app/test.component.ts
+++ src/app/test.component.ts
@@ -14,7 +14,7 @@ export class TestComponent
 {
 	text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non aliquam libero, euismod semper mauris. Donec in sollicitudin metus. Aenean non sem massa. Phasellus semper, ante vel fermentum efficitur, massa ex pulvinar arcu, vel semper enim erat eget enim. Maecenas tempor euismod eros";
 
-	@Input() isLiked:boolean = false;
+	@Input('isLK') isLiked:boolean = false;
 	stringInput:string;
 
 	like()
